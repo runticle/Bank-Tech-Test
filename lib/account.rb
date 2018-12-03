ba0ebfec5df3f2a9a4c376ff0fdb2f1fbef177b8
Alfie Freeman <runticle@gmail.com>
2018-12-03 11:20:05 +0000

    refactors account class for SRP

--- lib/account.rb
+++ lib/account.rb
@@ -1,20 +1,32 @@
 class Account
+
+  attr_reader :balance, :transactions
+
   def initialize
     @balance = 0
     @transactions = []
   end
 
-  attr_reader :balance, :transactions
-
   def deposit(amount)
     @balance += amount
-    @transactions.push({timestamp: Time.now, credit: amount, debit: 0, balance: @balance})
+    add_deposit_to_tx(amount)
   end
 
   def withdraw(amount)
     msg = 'You do not have enough money to withdraw this amount'
     raise msg if (@balance - amount) < 0
     @balance -= amount
+    add_withdraw_to_tx(amount)
+  end
+
+
+  private
+
+  def add_deposit_to_tx(amount)
+    @transactions.push({timestamp: Time.now, credit: amount, debit: 0, balance: @balance})
+  end
+
+  def add_withdraw_to_tx(amount)
     @transactions.push({timestamp: Time.now, credit: 0, debit: amount, balance: @balance})
   end
 
