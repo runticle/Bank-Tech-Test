ac3a34026ade7d68435c5351c6533bd06c3cd66f
Alfie Freeman <runticle@gmail.com>
2018-12-03 15:39:09 +0000

    refactors stubs in printer_spec

--- spec/printer_spec.rb
+++ spec/printer_spec.rb
@@ -9,22 +9,19 @@ describe Printer do
     Timecop.return
   end
 
+  let (:transaction) {double(timestamp: Time.now.strftime('%d/%m/%Y'),
+    credit: 100.0, debit: 0.0, balance: 100.0)}
+  let (:transaction2) {double(:transaction, balance: 50.0,
+    debit: 50.0, credit: 0.0, timestamp: Time.now.strftime('%d/%m/%Y'))}
+
   describe '#statement' do
     it 'prints the transactions in the desired format' do
-      transaction = double(timestamp: Time.now.strftime('%d/%m/%Y'),
-                           credit: 100.0, debit: 0.0, balance: 100.0)
-
       hd = 'Date || Credit || Debit || Balance'
       tx = "01/01/2000 || 100.0 || 0.0 || 100.0\n"
       expect { described_class.statement([transaction]) }.to output(hd + "\n" + tx).to_stdout
     end
 
     it 'prints the transactions in reverse order' do
-      transaction = double(timestamp: Time.now.strftime('%d/%m/%Y'),
-                           credit: 100.0, debit: 0.0, balance: 100.0)
-      transaction2 = double(:transaction, balance: 50.0,
-                                          debit: 50.0, credit: 0.0, timestamp: Time.now.strftime('%d/%m/%Y'))
-
       hd = 'Date || Credit || Debit || Balance'
       tx = "01/01/2000 || 0.0 || 50.0 || 50.0\n01/01/2000 || 100.0 || 0.0 || 100.0\n"
       expect { described_class.statement([transaction, transaction2]) }.to output(hd + "\n" + tx).to_stdout
