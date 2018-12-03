683bec40009dbb3e91e5b985e474eb6f1cbf4d21
Alfie Freeman <runticle@gmail.com>
2018-12-03 15:35:25 +0000

    refactors printer class to accomodate new transaction class format

--- lib/printer.rb
+++ lib/printer.rb
@@ -3,11 +3,7 @@ class Printer
   def self.statement(transactions)
     puts 'Date || Credit || Debit || Balance'
     transactions.reverse_each do |tx|
-      timestamp = tx[:timestamp]
-      credit = tx[:credit]
-      debit = tx[:debit]
-      balance = tx[:balance]
-      puts "#{timestamp} || #{credit} || #{debit} || #{balance}"
+      puts "#{tx.timestamp} || #{tx.credit} || #{tx.debit} || #{tx.balance}"
     end
   end
 end
--- spec/printer_spec.rb
+++ spec/printer_spec.rb
@@ -1,7 +1,7 @@
 require 'printer'
 
 describe Printer do
-  before do
+  before(:each) do
     Timecop.freeze(Time.local(2000))
   end
 
@@ -9,28 +9,25 @@ describe Printer do
     Timecop.return
   end
 
-  let(:tx_1) do
-    [{ timestamp: Time.now.strftime('%d/%m/%Y'),
-       credit: 100.0, debit: 0.0, balance: 100.0 }]
-  end
-  let(:tx_2) do
-    [{ timestamp: Time.now.strftime('%d/%m/%Y'),
-       credit: 100.0, debit: 0.0, balance: 100.0 },
-     { timestamp: Time.now.strftime('%d/%m/%Y'),
-       credit: 0.0, debit: 50.0, balance: 50.0 }]
-  end
-
   describe '#statement' do
     it 'prints the transactions in the desired format' do
+      transaction = double(timestamp: Time.now.strftime('%d/%m/%Y'),
+                           credit: 100.0, debit: 0.0, balance: 100.0)
+
       hd = 'Date || Credit || Debit || Balance'
       tx = "01/01/2000 || 100.0 || 0.0 || 100.0\n"
-      expect { described_class.statement(tx_1) }.to output(hd + "\n" + tx).to_stdout
+      expect { described_class.statement([transaction]) }.to output(hd + "\n" + tx).to_stdout
     end
 
     it 'prints the transactions in reverse order' do
+      transaction = double(timestamp: Time.now.strftime('%d/%m/%Y'),
+                           credit: 100.0, debit: 0.0, balance: 100.0)
+      transaction2 = double(:transaction, balance: 50.0,
+                                          debit: 50.0, credit: 0.0, timestamp: Time.now.strftime('%d/%m/%Y'))
+
       hd = 'Date || Credit || Debit || Balance'
       tx = "01/01/2000 || 0.0 || 50.0 || 50.0\n01/01/2000 || 100.0 || 0.0 || 100.0\n"
-      expect { described_class.statement(tx_2) }.to output(hd + "\n" + tx).to_stdout
+      expect { described_class.statement([transaction, transaction2]) }.to output(hd + "\n" + tx).to_stdout
     end
   end
 end
