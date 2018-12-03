f426388e12cd81a5806a13b510c9b71ec5c03b28
Alfie Freeman <runticle@gmail.com>
2018-12-03 11:11:53 +0000

    passes test for timestamp to deposits

--- lib/account.rb
+++ lib/account.rb
@@ -1,18 +1,19 @@
 class Account
   def initialize
     @balance = 0
+    @transactions = []
   end
 
-  attr_reader :balance
+  attr_reader :balance, :transactions
 
   def deposit(amount)
     @balance += amount
+    @transactions.push({timestamp: Time.now, credit: amount, debit: 0, balance: @balance})
   end
 
   def withdraw(amount)
     msg = 'You do not have enough money to withdraw this amount'
     raise msg if (@balance - amount) < 0
-
     @balance -= amount
   end
 end
